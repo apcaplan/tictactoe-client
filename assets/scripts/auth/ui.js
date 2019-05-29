5b2c2c59c38fc40a6bcc0dcff91b6b8b0c7a6c66
apcaplan <apcaplan@gmail.com>
2019-05-29 16:04:33 -0400

    Update error messages and styling

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,16 +2,16 @@
 const store = require('../store')
 
 const onSignUpSuccess = responseData => {
-  $('.error').show()
-  $('.error').text('Created password successfully!').fadeOut(2500)
+  $('.earlyError').show()
+  $('.earlyError').html('Created password successfully!').fadeOut(2500)
   $('#sign-up').hide()
   $('#sign-in').show()
   $('#change-password').get(0).reset()
 }
 
 const onSignUpFailure = responseData => {
-  $('.error').show()
-  $('.error').text('Couldn\'t register with this email and password. Please try again! ').fadeOut(2500)
+  $('.earlyError').show()
+  $('.earlyError').html('Couldn\'t register with this email and password. Please try again!').fadeOut(3500)
   $('#sign-up').get(0).reset()
 }
 
@@ -28,8 +28,8 @@ const onSignInSuccess = responseData => {
 }
 
 const onSignInFailure = responseData => {
-  $('.error').show()
-  $('.error').text('Could not sign in. Please check email address and password, and try again - or create a new account.').fadeOut(2500)
+  $('.earlyError').show()
+  $('.earlyError').text('Could not sign in. Please check email address and password, and try again - or create a new account.').fadeOut(3500)
   $('#sign-in').get(0).reset()
 }
 
@@ -49,13 +49,13 @@ const onChangePwFailure = responseData => {
 
 const onLogoutSuccess = () => {
   $('.messages').show()
-  $('.messages').text('Thanks for playing! Come back again soon :)').fadeOut(2500)
-  setTimeout(() => $('.opening').show(), 2800)
+  $('.messages').html('Thanks for playing! Come back again soon :)').fadeOut(2500)
+  setTimeout(() => $('.opening').show(), 3000)
 }
 
 const onLogoutFailure = () => {
   $('.messages').show()
-  $('.messages').text('Couldn\'t log out :()').fadeOut(2500)
+  $('.messages').html('Couldn\'t log out :()').fadeOut(3500)
 }
 
 module.exports = {
